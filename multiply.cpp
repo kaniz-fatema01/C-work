ef465229b312dd6b396ffe58f1bb3e46e9eb8a0e
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-09 20:25:01 +0600

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+   printf("a: ");
+   scanf("%d", &a);
+   int b;
+   printf("b: ");
+   scanf("%d",&b);
+   int mul;
+   mul= a*b;
+   printf("mul:%d ",mul);
+
+
+return 0;
+}
