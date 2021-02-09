b90cd5dbd7af89a1d93fa1c81e2b442c2ba37627
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-09 20:22:23 +0600

    Create subtract.cpp

---
+++ subtract.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+   printf("a: ");
+   scanf("%d", &a);
+   int b;
+   printf("b: ");
+   scanf("%d",&b);
+   int sub;
+   sub= a-b;
+   printf("sub:%d ",sub);
+
+
+return 0;
+}
