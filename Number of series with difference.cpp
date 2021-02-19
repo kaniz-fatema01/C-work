df6b7fb678aa0b5a90a069ce80b8ed82a257cc38
Kaniz Fatema <78794903+kaniz-fatema01@users.noreply.github.com>
2021-02-19 13:21:40 +0600

    Create Number of series with difference.cpp

---
+++ Number of series with difference.cpp
@@ -0,0 +1,22 @@
+//Number of series with difference
+#include<stdio.h>
+
+int main()
+{
+    int a,b,i,Result;
+   printf("How many number of sum do you want: ");
+   scanf("%d", &a);
+   printf("Difference between numbers: ");
+   scanf("%d", &b);
+   Result=0;
+
+   for(i=1;i<=a;i++)
+   {
+       Result=Result+b;
+       printf("%d ",Result );
+
+   }
+
+
+return 0;
+}
